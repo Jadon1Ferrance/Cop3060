9e70e2e0d3246c8528118f20235ae228083dc0f8
jadon1.ferrance <97418571+Jadon1Ferrance@users.noreply.github.com>
2025-10-28 20:17:41 -0400

    Add ResourceDTO record

---
+++ src/main/java/edu/famu/cop3060/resources/src/main/java/edu/famu/cop3060/resources/dto/ResourceDTO.java
@@ -0,0 +1,14 @@
+package edu.famu.cop3060.resources.dto;
+
+
+import java.util.List;
+
+
+public record ResourceDTO(
+  String id,
+  String name,
+  String category,
+  String location,
+  String url,
+  List<String> tags
+) {}
