7eacfdf319083a2dee8465bc8f2af8e4b88e4cef
jadon1.ferrance <97418571+Jadon1Ferrance@users.noreply.github.com>
2025-10-28 20:10:17 -0400

    Create ResourcesApplication for Spring Boot app
    add Spring Boot entry point

---
+++ src/main/java/edu/famu/cop3060/resources/ResourcesApplication.java
@@ -0,0 +1,13 @@
+package edu.famu.cop3060.resources;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ResourcesApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ResourcesApplication.class, args);
+  }
+}
