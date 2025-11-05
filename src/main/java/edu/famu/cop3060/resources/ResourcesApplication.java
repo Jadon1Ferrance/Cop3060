4b83b0c5b12e7a864ab4a8a741f825692eb4874f
jadon1.ferrance <97418571+Jadon1Ferrance@users.noreply.github.com>
2025-11-04 21:11:39 -0500

    Add logging to ResourcesApplication startup

--- src/main/java/edu/famu/cop3060/resources/ResourcesApplication.java
+++ src/main/java/edu/famu/cop3060/resources/ResourcesApplication.java
@@ -1,13 +1,17 @@
 package edu.famu.cop3060.resources;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ResourcesApplication {
+  private static final Logger log = LoggerFactory.getLogger(ResourcesApplication.class);
+
   public static void main(String[] args) {
     SpringApplication.run(ResourcesApplication.class, args);
+    log.info("Campus Resources API (in-memory) started.");
   }
 }
+
